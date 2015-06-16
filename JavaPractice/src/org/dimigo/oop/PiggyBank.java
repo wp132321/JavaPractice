243439c3a6a6e2c6c2d670f904e640c84e08e801
wp132321 <tap2930@icloud.com>
2015-06-16 11:47:35 +0900

    PiggyBank

---
+++ JavaPractice/src/org/dimigo/oop/PiggyBank.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *    |_ PiggyBank
+ *
+ *   1. 개요 :
+ *   2. 작성일 : 2015. 6. 16.
+ *  </pre>
+ *
+ * @author      : 이명현
+ * @version     : 1.0
+ * @github      : http://github.com/wp132321
+ */
+
+public class PiggyBank {
+
+	private static int balance;
+	
+	public static void putMoney(FamilyMember member, int amount) {
+		System.out.println(String.format("%s %d원 넣음", member.getMemberName(), amount));
+		balance += amount;
+	}
+	
+	public static void printBalance() {
+		System.out.println(String.format("돼지저금통 총 금액 : %d원", balance));
+	}
+}
---
+++ JavaPractice/src/org/dimigo/oop/PiggyBankTest.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *    |_ PiggyBankTest
+ *
+ *   1. 개요 :
+ *   2. 작성일 : 2015. 6. 16.
+ *  </pre>
+ *
+ * @author      : 이명현
+ * @version     : 1.0
+ * @github      : http://github.com/wp132321
+ */
+
+public class PiggyBankTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FamilyMember[] family = {
+				new FamilyMember("아빠"), new FamilyMember("엄마"), new FamilyMember("나"), new FamilyMember("남동생")
+		};
+		
+		FamilyMember.printMemberCnt();
+		
+		PiggyBank.putMoney(family[0], 10000);
+		PiggyBank.putMoney(family[1], 5000);
+		PiggyBank.putMoney(family[2], 2000);
+		PiggyBank.putMoney(family[3], 1000);
+		
+		PiggyBank.printBalance();
+		
+		PiggyBank.putMoney(family[2], 1000);
+		
+		PiggyBank.printBalance();
+	}
+
+}
